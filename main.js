eea3bfdcd24bb12b41fc1bc07d2992eb27861356
zakwan93 <zakwan_bhaiyat@yahoo.com>
2018-03-22 23:50:31 -0400

    Change and add more styles to the main page

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.getElementById('emailme').addEventListener('click',function(){
 
 
 $('#switch1').on('click', function(){
-	$('body').toggleClass('night')
+  $('body').toggleClass('night')
 })
 
 // $("#emojiwavehand").click(function() {
@@ -21,14 +21,14 @@ $('#switch1').on('click', function(){
 
 
 $(document).ready(function(){
-	var d = new Date();
-	var n = d.getHours();
-	if (n > 19 || n < 6)
-	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
-	  document.body.className = "night";
-	else
-	  // Else use ‘day’ theme
-	  document.body.className = "";
+  var d = new Date();
+  var n = d.getHours();
+  if (n > 19 || n < 6)
+    // If time is after 7PM or before 6AM, apply night theme to ‘body’
+    document.body.className = "night";
+  else
+    // Else use ‘day’ theme
+    document.body.className = "";
 });
 
 $(window).load(function() {
@@ -38,32 +38,32 @@ $(window).load(function() {
 $(window).scroll(function(){
   var threshold = 200; // number of pixels before bottom of page that you want to start fading
   var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
-	if( op <= 18 ){
-		$("#top-button").show();
-	} else {
-		$("#top-button").hide();
-	}
+  if( op <= 18 ){
+    $("#top-button").show();
+  } else {
+    $("#top-button").hide();
+  }
 });
 
 
 
 
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $("section").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
+// $(window).on("load",function() {
+//   $(window).scroll(function() {
+//     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+//     $("section").each(function() {
+//       /* Check the location of each desired element */
+//       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
-       If the element is completely within bounds of the window, fade it in 
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(200,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(250,0);}
-      }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
+//        If the element is completely within bounds of the window, fade it in 
+//       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+//         if ($(this).css("opacity")==0) {$(this).fadeTo(200,1);}
+//       } else { //object goes out of view (scrolling up)
+//         if ($(this).css("opacity")==1) {$(this).fadeTo(250,0);}
+//       }
+//     });
+//   }).scroll(); //invoke scroll-handler on page-load
+// });
 
 
 
