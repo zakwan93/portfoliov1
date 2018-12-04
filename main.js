e81c01170c591e169b0c0e85dd150a6148ee4a29
zakwan93 <zakwan_bhaiyat@yahoo.com>
2018-12-04 16:31:22 -0500

    Redesignd the whole Portfolio with some animation

--- main.js
+++ main.js
@@ -1,86 +1,33 @@
 
-// var TxtRotate = function(el, toRotate, period) {
-//   this.toRotate = toRotate;
-//   this.el = el;
-//   this.loopNum = 0;
-//   this.period = parseInt(period, 10) || 2000;
-//   this.txt = '';
-//   this.tick();
-//   this.isDeleting = false;
-// };
+// On Click hand emoji will take you to the top of the page
+document.getElementById('top-button').addEventListener('click',function(){
+    window.scrollTo(0,0);
+});
 
-// TxtRotate.prototype.tick = function() {
-//   var i = this.loopNum % this.toRotate.length;
-//   var fullTxt = this.toRotate[i];
-
-//   if (this.isDeleting) {
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//   } else {
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//   }
-
-//   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-//   var that = this;
-//   var delta = 300 - Math.random() * 100;
-
-//   if (this.isDeleting) { delta /= 2; }
-
-//   if (!this.isDeleting && this.txt === fullTxt) {
-//     delta = this.period;
-//     this.isDeleting = true;
-//   } else if (this.isDeleting && this.txt === '') {
-//     this.isDeleting = false;
-//     this.loopNum++;
-//     delta = 500;
-//   }
-
-//   setTimeout(function() {
-//     that.tick();
-//   }, delta);
-// };
-
-// window.onload = function() {
-//   var elements = document.getElementsByClassName('txt-rotate');
-//   for (var i=0; i<elements.length; i++) {
-//     var toRotate = elements[i].getAttribute('data-rotate');
-//     var period = elements[i].getAttribute('data-period');
-//     if (toRotate) {
-//       new TxtRotate(elements[i], JSON.parse(toRotate), period);
-//     }
-//   }
-//   // INJECT CSS
-//   var css = document.createElement("style");
-//   css.type = "text/css";
-//   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-//   document.body.appendChild(css);
-// };
- 
+$(window).scroll(function(){
+  var threshold = 208; // number of pixels before bottom of page that you want to start fading
+  var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
+  if( op <= 13 ){
+    $("#top-button").show();
+  } else {
+    $("#top-button").hide();
+  }
+});
 
 
 
-$('#right-arrow').on('click', function(){
 
-  $('#intro').toggleClass('#background')
-})
-
-
-
+// Toggle between DAy and Night Mode
 $('#switch1').on('click', function(){
   $('body').toggleClass('night')
 })
 
 
-// $("#emojiwavehand").click(function() {
-//   // not gonna work
-//   $(this).removeClass(".intro_hello .wave-hand.animated").addClass(".intro_hello .wave-hand.animated");
-// });
-// $("body").delay(2000);
-
+// If user visit after 7 pm in night the body will change the style to night class
 $(document).ready(function(){
-  var d = new Date();
-  var n = d.getHours();
-  if (n > 19 || n < 6)
+  var date = new Date();
+  var current_time = date.getHours();
+  if (current_time > 19 || current_time < 6)
     // If time is after 7PM or before 6AM, apply night theme to ‘body’
     document.body.className = "night";
   else
@@ -88,25 +35,12 @@ $(document).ready(function(){
     document.body.className = "";
 });
 
-// $(window).load(function() {
-//       $("#top-button").hide();
-// });
-
-$(window).scroll(function(){
-  var threshold = 298; // number of pixels before bottom of page that you want to start fading
-  var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
-  if( op <= 18 ){
-    $("#top-button").show();
-  } else {
-    $("#top-button").hide();
-  }
+$(window).load(function() {
+      $("#top-button").hide();
 });
 
-// $( ".project_used span" ).hover(function() {
-//   $( this ).fadeOut( 'fast' );
-//   $( this ).fadeIn( 'slow' );
-// });
 
+// If you hover over the languages I used in project Box it will do fade in animation
 $('.project_used span').on({
     mouseover: function() {
         event.preventDefault();
@@ -119,6 +53,7 @@ $('.project_used span').on({
 });
 
 
+// Body will add and remove Fade-In animation after scrolling to Y axis at some point
 var sections = document.getElementsByTagName("section");
 var section1 = sections[0];
 var section2 = sections[1];
@@ -126,99 +61,49 @@ var section3 = sections[2];
 var section4 = sections[3];
 var section5 = sections[4];
 
-window.addEventListener("scroll",function(e){
-  if(window.scrollY > 200 || window.scrollY > 150){
+
+window.addEventListener("scroll",function(){
+  if(window.scrollY > 200 ){
     section1.classList.add("fadeIn")
   }
 
-  if(window.scrollY > 1050){
+  if(window.scrollY > 1250){
     section1.classList.remove("fadeIn")
   }
 
-  if(window.scrollY > 900 || window.scrollY > 1150){
+  if(window.scrollY > 900 ){
     section2.classList.add("fadeIn")
   }
 
-  if(window.scrollY > 1200){
+  if(window.scrollY > 1800){
     section2.classList.remove("fadeIn")
   }
 
-  if (window.scrollY > 1350 || window.scrollY > 1700){
+  if (window.scrollY > 1450 ){
     section3.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 1800 ){
+  if (window.scrollY > 2600 ){
     section3.classList.remove("fadeIn")
   }
 
-  if (window.scrollY > 1800){
+  if (window.scrollY > 2450){
     section4.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 3550 || window.scrollY < 1779){
+  if (window.scrollY > 3500){
     section4.classList.remove("fadeIn")
   }
 
-  if (window.scrollY > 3600 ){
+  if (window.scrollY > 3150 ){
     section5.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 5850 || window.scrollY < 3599){
+  if (window.scrollY > 4000 ){
     section5.classList.remove("fadeIn")
   }
 })
 
-
-
-
-
-
-
-
-// $(document).ready(function () {
-//     $('header.intro').fadeIn(3200);
-// });
-
-
-// $(document).ready(function() {
-//     $('.featured-projects-content').hide();
-//     $('.featured-projects .project_pic img').animate({
-//         opacity:1
-        
-//     });
-
-//     $('featured-projects .project_pic img').hover(function() {
-//         $(this).stop().animate({opacity:.4},200);
-//         $('.featured-projects-content').fadeIn();
-
-//     }, function() {
-//         $(this).stop().animate({opacity:1},500)
-//         $('.featured-projects-content').fadeOut();
-//     });
-
-// });
-
-
-
-// $(document).ready(function(){
-
-//   $(window).scroll( function(){
-
-//       $('section').each(function(i){
-//          var bottom_of_object = $(this).offset().top + $(this).innerHeight();
-//             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-//             /* If the object is completely visible in the window, fade it it */
-//             if( bottom_of_window >= bottom_of_object ){
-                
-//                 $(this).animate({'opacity':'1'},500);
-                    
-//             }
-            
-//           });
-//   });
-
-// });
 
 
 
